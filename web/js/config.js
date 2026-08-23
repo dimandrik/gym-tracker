36151a78674cc9b24b9c01ee5dc365611b47ae44
dimandrik <dmitry.solodov07@gmail.com>
2026-08-23 17:25:11 +0600

    Defining API_BASE_URL depending on the environment

--- web/js/config.js
+++ web/js/config.js
@@ -1,1 +1,7 @@
-const API_BASE_URL = 'http://' + window.location.hostname + ':8080';
+let API_BASE_URL;
+
+if (window.location.hostname === 'gym-tracker.dev') {
+    API_BASE_URL = window.location.origin;
+} else {
+    API_BASE_URL = 'http://' + window.location.hostname + ':8080';
+}
