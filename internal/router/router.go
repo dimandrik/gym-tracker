93dc497a5b2ba1112cb9ccccb3e41e5a46c4de71
dimandrik <dmitry.solodov07@gmail.com>
2026-08-24 15:59:25 +0600

    The health check has been moved to /api/healthz

--- internal/router/router.go
+++ internal/router/router.go
@@ -45,9 +45,9 @@ func New(
 	mux.HandleFunc("PUT /api/user/email", authMiddleware(userHandler.UpdateEmail))
 	mux.HandleFunc("PUT /api/user/password", authMiddleware(userHandler.UpdatePassword))
 	mux.HandleFunc("DELETE /api/user/account", authMiddleware(userHandler.DeleteAccount))
-	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte("ok"))
+		_ = w.Write([]byte("ok"))
 	})
 
 	mux.Handle("/uploads/", http.StripPrefix("/uploads/", http.FileServer(http.Dir("uploads"))))
